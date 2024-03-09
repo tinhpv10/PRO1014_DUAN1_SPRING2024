0b01fa47d9b86c8fc8c3aaa5c1d2d924f70a033e
TinhPV10 <tinhpv10@fpt.edu.vn>
2024-03-09 20:27:10 +0700

    [HTML] - Action button

---
+++ learning/particals/action-buttons.php
@@ -0,0 +1,25 @@
+<div class="list-action">
+    <div class="button-contact">
+        <div class="phone-vr">
+            <div class="phone-vr-circle-fill"></div>
+            <div class="phone-vr-img-circle">
+                <a href="tel:0366999997">
+                    <img alt="Phone" src="https://khoithanhgroup.com/wp-content/plugins/button-contact-vr/img/phone.png">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="button-contact">
+        <div class="phone-vr facebook-vr">
+            <div class="phone-vr-circle-fill"></div>
+            <div class="phone-vr-img-circle">
+                <a href="tel:0366999997">
+                    <img alt="Phone" src="https://khoithanhgroup.com/wp-content/plugins/button-contact-vr/img/phone.png">
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+</div>
