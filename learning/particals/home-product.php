b3ad88e0935ac24aaa9db88b9f9dbc68a8ab6519
TinhPV10 <tinhpv10@fpt.edu.vn>
2024-03-09 18:35:34 +0700

    [HTML] - Home product, footer

---
+++ learning/particals/home-product.php
@@ -0,0 +1,25 @@
+<div class="container">
+
+    <div class="section-product py-5">
+
+        <h4 class="title-section">Sản phẩm nổi bật</h4>
+
+        <div class="row">
+
+            <?php for ($i = 0; $i < 12; $i++) : ?>
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                    <div class="card mb-4">
+                        <img src="http://ltfood.vn/storage/01HJFD3YJD0ZRTJABJJ2JFBPFA.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endfor; ?>
+
+        </div>
+    </div>
+
+</div>
